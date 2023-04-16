8ae218185176fc46233a21fe82d5f0e2298551e1
Lodeon <minecraft.log02z@gmail.com>
2023-04-16 15:13:53 +0200

    Modified TODO list

--- Library/Graphics/GraphicCanvas.cs
+++ Library/Graphics/GraphicCanvas.cs
@@ -9,12 +9,10 @@
             _buffer = buffer;
         }
 
+        public ReadonlyGraphicBuffer AsReadonly()
+            => new ReadonlyGraphicBuffer(_buffer);
+
         public void Fill(Color color)
             => _buffer.Fill(new Pixel().WithBackground(color));
-
-        public ReadonlyGraphicBuffer AsReadonly()
-        {
-            return new ReadonlyGraphicBuffer(_buffer);
-        }
     }
 }
--- Test Applications/ConsoleApp1/Program.cs
+++ Test Applications/ConsoleApp1/Program.cs
@@ -2,30 +2,30 @@
 using Lodeon.Terminal.UI.Layout;
 
 /*  TODO:
-    - Layout stacking algorithm
+    [#] Layout stacking algorithm
 
-    - Layout property change updates graphics and recalculates LayoutResults
+    [#] Layout property change updates graphics and recalculates LayoutResults
 
-    [-] Hide output driver from Element objects, add support for display functions that take in element's graphic buffer and merge it
+    [!] <Hide output driver from Element objects>, add support for display functions that take in element's graphic buffer and merge it
         . with all parent and child buffers
 
-    - LayoutElement TreeFromXML with attribute for naming
+    [#] LayoutElement TreeFromXML with attribute for naming
 
-    - Event based input system for Element objects. EVent Propagation: (Program > Page > Selected Element)
+    [#] Event based input system for Element objects. EVent Propagation: (Program > Page > Selected Element)
         . allow for returning a bool in input callback to stop inputs from propagating to page or items
 
-    - Add functions to draw shapes on grahpic buffers using source coordinates and/or precentages. Maybe use wrapper to prevent
-        . buffer size and position change
+    [!] Add functions to draw shapes on grahpic buffers using source coordinates and/or precentages. <Maybe use wrapper to prevent
+        . buffer size and position change>
 
-    - Implement event on Driver class that fires whenever output window's size changes. Not sure on how to force it
+    [#] Implement event on Driver class that fires whenever output window's size changes. Not sure on how to force it
     
-    - Add reference to current Program in Element and Page class so they can access information like program's name.
+    [#] Add reference to current Program in Element and Page class so they can access information like program's name.
         . May be better to wrap in a ProgramInfo class
 
-    - Impelent page changing program and make it so it can be called by elements and pages.
+    [#] Impelent page changing program and make it so it can be called by elements and pages.
         . Make abstract Main() and OnExit() methods on Page that will be called by the program when page changes
 
-    - Implement methods to get items in a page by type, by id or both
+    [#] Implement methods to get items in a page by type, by id or both
  */
 
 await Script.Run<MyProgram>();

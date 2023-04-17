0244680515682ecc2d7bd52f74f80ffa7b1491ee
Lodeon <91905473+Lodeon003@users.noreply.github.com>
2023-04-17 08:48:26 +0200

    Added methods to draw rectangle on GraphicCanvas class

--- Library/Graphics/GraphicCanvas.cs
+++ Library/Graphics/GraphicCanvas.cs
@@ -1,18 +1,29 @@
-﻿namespace Lodeon.Terminal.Graphics
+﻿using System.Drawing;
+
+namespace Lodeon.Terminal.Graphics;
+
+public class GraphicCanvas
 {
-    public class GraphicCanvas
+    private GraphicBuffer _buffer;
+    
+    public GraphicCanvas(GraphicBuffer buffer)
     {
-        private GraphicBuffer _buffer;
+        _buffer = buffer;
+    }
 
-        public GraphicCanvas(GraphicBuffer buffer)
-        {
-            _buffer = buffer;
-        }
+    public ReadonlyGraphicBuffer AsReadonly()
+        => new ReadonlyGraphicBuffer(_buffer);
 
-        public ReadonlyGraphicBuffer AsReadonly()
-            => new ReadonlyGraphicBuffer(_buffer);
+    public void Fill(Color color)
+        => _buffer.Fill(new Pixel().WithBackground(color));
 
-        public void Fill(Color color)
-            => _buffer.Fill(new Pixel().WithBackground(color));
+    public void DrawRectangle(Rectangle area, Color background)
+        => DrawRectangle(area, new Pixel().WithBackground(background));
+
+    public void DrawRectangle(Rectangle area, Pixel pixel)
+    {
+        for(int y = area.Top; y < area.Bottom; y++)
+            for(int x = area.Left; x < area.Right; x++)
+                _buffer.Write(x, y, pixel);
     }
 }
